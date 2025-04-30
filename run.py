33aa8df81832d0d09a90691302545da351467ec8
Jayden Dave <jaydendave@hotmail.co.uk>
2025-04-30 14:11:45 +0100

    added run

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from models import Goal 
+import pandas as pd
 
+dataset = pd.read_csv("simulated family savings contributions.csv")
+members_list = ['Frank', 'Nancy', 'Mark', 'Abigail']
+holiday_goal = Goal(800, 5, 'Family Holiday', members_list)
 
-holiday_goal = Goal(800, 5, 'Family Holiday')
-
-for row in dataset:
+for index, row in dataset.iterrows():
+    print(row)
     holiday_goal.contribute(row)
     holiday_goal.indiv_contribution(row)
+
